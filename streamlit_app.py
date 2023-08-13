a5ff120fa076fdb762ba238e6003e33dd0df82dd
oksuzzz <142138858+oksuzzz@users.noreply.github.com>
2023-08-13 10:38:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-import ploty.exprees as px
+import plotly.exprees as px
 st.title("ilk cloud streamlit projem")
 
 btc=yf.download("BTC-USD","2008-01-01","2023-08-13")
